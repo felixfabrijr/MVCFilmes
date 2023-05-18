0421e463ae2ed7a8e7e399f8d9f19e6890eb2c55
felix <felix@FABRI>
2023-05-18 04:41:27 +0100

    Add project files.

---
+++ MVCFilmes/Controllers/FilmesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCFilmes.Controllers
+{
+    public class FilmesController : Controller
+    {
+        public string Index()
+        {
+            return "Action Padrao";
+        }
+
+        public string Welcome() => "Seja bem vindo";
+    }
+}
